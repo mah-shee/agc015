a20c10fb7a7ed8d89318361db8f2c13b6d27a4a1
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-13 15:45:27 +0900

    done a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,5 +4,20 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        a: usize,
+        b: usize,
+    }
+    if a > b {
+        println!("0");
+    } else if a == b {
+        println!("1");
+    } else {
+        if n == 1 && a != b {
+            println!("0");
+        } else {
+            let ans = (b - a) * (n - 2) + 1;
+            println!("{}", ans);
+        }
     }
 }
