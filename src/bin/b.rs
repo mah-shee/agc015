582856a7dc79d78c06fd76c82aca1e15b396449b
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-19 00:30:35 +0900

    Done b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -4,5 +4,20 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        s: Chars,
     }
+    let f = s.len();
+    let mut ans = 2 * (f - 1);
+    if f == 3 {
+        println!("7");
+        return;
+    }
+    for i in 1..f - 1 {
+        if s[i] == 'U' {
+            ans += (f - i - 1) + i * 2;
+        } else {
+            ans += (f - i - 1) * 2 + i;
+        }
+    }
+    println!("{}", ans);
 }
